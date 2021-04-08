d6e34b59a93ef2bca0affa0b17cac84910567286
Eugene Lin <eugenelin89@gmail.com>
2021-04-07 21:32:58 -0700

    Use user_id to train

--- train.jl
+++ train.jl
@@ -1,7 +1,9 @@
-#using Pkg
-#Pkg.activate("env")
+# To start server, on command line, $julia train.jl
 
-using Genie, Genie.Router
+using Pkg
+Pkg.activate("env")
+
+using Genie, Genie.Router, Genie.Requests
 using Genie.Renderer, Genie.Renderer.Html, Genie.Renderer.Json
 
 println("Load Packages...")
@@ -9,9 +11,11 @@ using CSV
 using DataFrames
 using MLJ
 
-route("/train") do
+route("/train/:user_id") do
     println("Load CSV...")
-    df = DataFrame(CSV.File("christopher.csv", header=["id", "name","date_time","m1","m2","ans","time","cor"]))
+    filename = "data_raw/$(payload(:user_id)).csv.clean"
+    println(filename)
+    df = DataFrame(CSV.File(filename, header=["id", "name","date_time","m1","m2","ans","time","cor"]))
     df_time = df[:, 4:7]
     coerce!(df_time, Count => Continuous);
 
@@ -33,9 +37,22 @@ route("/train") do
     predicted_time = [pred[i].μ for i in 1:size(pred)[1]]  # THIS IS THE RESULT WE WANT
     X12.predicted_time = predicted_time
     sort!(X12, rev=true, [:predicted_time])
-    println(X12)
+    #println(X12)
     #html(X12)
+    CSV.write("data_trained/$(payload(:user_id)).trained.csv", X12)
     respond(string(X12), :text)
+end
+
+route("/hello.html") do
+    html("Hello World")
+end
+
+route("/hello.json") do
+    json("Hello World")
+end
+
+route("/hello.txt") do
+    respond("Hello World", :text)
 end
 
 up(8001, async = false)
